556b0880aeab68de3ffc52eb92a66acab7b14022
rlg0013 <abhideep0023@gmail.com>
2026-08-05 22:24:23 +0530

    added a newArray button and learnt more about react

--- src/pages/SortingPage.jsx
+++ src/pages/SortingPage.jsx
@@ -1,12 +1,31 @@
 import Bars from '../components/Bars';
 import Controls from '../components/Controls';
+import { useState} from 'react';
+
+function generateRandomArray(size) {
+  return Array.from({length: size}, ()=> Math.floor(Math.random()*100) + 5)
+}
+
 
 function SortingPage() {
-  const testArray = [5, 7, 8, 9, 1, 2, 4]
+  const [size, setSize] = useState(20)
+  const [array, setArray] = useState(() => generateRandomArray(size))
+
+  function handleSizeChange(newSize) {
+    setSize(newSize)
+    setArray(generateRandomArray(newSize))
+}
+
+  function handleNewArray() {
+    setArray(generateRandomArray(size))
+  }
   return (
     <div className='p-8'>
-      <Controls />
-      <Bars data={testArray} />
+      <Controls
+        size={size}
+        onNewArray={handleNewArray}
+        onSizeChange={handleSizeChange} />
+      <Bars data={array} />
     </div>
   )
 }
